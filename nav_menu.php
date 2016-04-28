b6fe7525c01f1575370a41b355fad06e8e106a31
jrn2477 <jrn2477@rit.edu>
2016-04-28 11:39:40 -0400

    Here is the PHP Menu for the Include

---
+++ nav_menu.php
@@ -0,0 +1,36 @@
+
+<!-- Sidebar -->
+<div id="sidebar-wrapper">
+<ul class="sidebar-nav">
+    <li class="sidebar-brand">
+        <a href="#">Tiger's Explore</a>
+    </li>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Nightlife</a></li>
+    <li><a href="#">Day Trips</a></li>
+    <li><a href="#">Food</a></li>
+    <li><a href="#">Support</a></li>
+</ul>
+</div>
+<!-- /#sidebar-wrapper -->
+
+<?php
+    echo "<div id='sidebar-wrapper'>";
+    echo "<ul class='sidebar-nav'>";
+
+    # This is the beggining of the home link 
+
+    echo "<li class='sidebar-brand'>";
+
+            echo "<a href='tiger_explore.php' class=' ";    
+            echo ($_SERVER['PHP_SELF'] == 'tiger_explore.php' ? ' active' : '');
+            echo "'>Tiger's Explore</a>";
+
+    echo "</li>";
+
+    # This is the echo statement for the first link 
+
+
+
+?>
