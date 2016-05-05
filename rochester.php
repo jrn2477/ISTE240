21626a1c789dec4fc39c9396d98e56b395e86b35
jrn2477 <jrn2477@rit.edu>
2016-05-05 11:26:11 -0400

    My first page
    Here is my first page. It is still being edited, but feel free to use the same type of styling

---
+++ rochester.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Team 1
+    ">
+
+    <title>Tigers Explore Rochester</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+    <!-- Flamenco Font --> 
+    <link href='https://fonts.googleapis.com/css?family=Flamenco' rel='stylesheet' type='text/css'>
+    <!-- Volkorn Font --> 
+    <link href='https://fonts.googleapis.com/css?family=Vollkorn' rel='stylesheet' type='text/css'>
+    <!-- Custom Stylesheet --> 
+    <link href="css/custom-style.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+        
+        <!-- Sidebar 
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">Tiger's Explore</a>
+                </li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Nightlife</a></li>
+                <li><a href="#">Day Trips</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Support</a></li>
+            </ul>
+        </div>
+        /#sidebar-wrapper -->
+
+        <?php 
+            include "nav_menu.php"; 
+        ?>
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+
+            <!-- Header --> 
+            <div class="row">
+            <div class="container-fluid"> 
+                <div class="container-fluid col-lg-1">
+                    <!-- menu-button --> 
+                    <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">
+                        <span class="glyphicon glyphicon-menu-hamburger"></span>
+                    </a>
+                </div><!-- /menu-button --> 
+                <div class="11">
+                    <!-- Site Title & Subtitle --> 
+                    <h1 id="top"><!-- Start Title--><b>Tiger's Explore </b><!-- End Title-->
+                    <!-- Start subtitle-->Rochester<!-- End Subtitle-->
+                    </h1>
+                </div>
+            </div>
+            </div>
+            <!-- End Header--> 
+
+
+            <div class="container">
+            
+
+
+            
+                <div class="container-fluid"> 
+                    <div class="row">
+                    <div class="sec_menu col-12">
+                        <ul>
+                            <li class="sm_item"><a class="sec_menu_links"href="#">Rochester</a></li>
+                            <li class="sm_item"><a class="sec_menu_links"href="#">Syracuse</a></li>
+                            <li class="sm_item"><a class="sec_menu_links"href="#">Buffalo</a></li>
+                        </ul>
+                    </div>
+                    </div>
+
+
+
+                        <div class="row">
+                                                
+                        <div class="row">
+                        <div class="col-xs-12 col-lg-10">
+
+                            <div class="row">
+                            <h2 id="Cobbs">Cobb's Hill</h2>
+                            </div>
+                            
+                            <div class="row">
+                            <div class="container col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                <figure>
+                                    <img class="sm_img" width="90%" src="img/cobbshill.jpg" alt="Cobb's Hill Sunset">
+                                    <figcaption><a href="http://media.democratandchronicle.com/content/cobbs-hill-reservoir">Image Source</a></figcaption>
+                                </figure>
+                            </div>
+                            </div>
+                            
+                            <!-- Cobb's Hill Park Description Start --> 
+                            <div class="row ch_desc">
+                            <h3>About Cobb's Hill</h3>
+
+                            <p>Cobb's Hill is a part located toward the south-east end of the city (the part closest to RIT). The park is apprximately 109 acres. Inside the park you will find a variety of walking trails, beautiful forestry including lilac trees.</p>
+
+                            <p>Cobb's Hill is a favorite spot for runners, who like to do laps around the park's water filled basin, as well as Lake Riley, which can be found at the park's entrance, and which also attracts sunbathers in the warmer months.</p>
+
+                            <p>At the East side of the reservoir, visitors of the park wll find 100 acres of undeveloped natural beauty, right in the heart of the city. Walking trails have been sprinkled throughout the park to allow joggers, dog-walkers and other visitors of the park to meander through the park's beautiful trails. </p>
+                            </div>
+                            <!-- End Cobb's Hill Park Description  --> 
+
+
+                        <!-- New Row for the Park Information --> 
+                        <div class="row">
+                        <div class="col-xs-12 col-lg-4">
+                            <h3>Facilities Available:</h3>
+
+                            <ul>
+                                <li>Ball Fields</li>
+                                <li>Outdoor BasketBall Courts</li>
+                                <li>Cross-country skiiing</li>
+                                <li>Fishing</li>
+                                <li>Football/Soccer</li>
+                                <li>Playground</li>
+                                <li>Tennis Courts</li>
+                                <li>Hiking Trails</li>
+                            </ul>
+                        </div>
+
+                        <div class="col-xs-12 col-lg-4">
+                            <h3>Park Information:</h3>
+
+                            <ul>
+                                <li>Phone: 585-428-6770</li>
+                                <li>Address: 100 Noris Drive, Rochester NY 14623</li>
+                            </ul>
+                        </div>
+
+                        <div class="col-xs-12 col-lg-4">
+                            <h3>Hours of Operation:</h3>
+
+                            <ul>
+                                <li>Summer Months (4/1 - 11/15):
+                                <ul>
+                                    <li>5:00am - 11:00pm</li> 
+                                </ul> 
+                                </li>
+                                <li>Winter Months (11/16 - 3/30):
+                                <ul>
+                                    <li>7:00am - 10:00pm</li> 
+                                </ul> 
+                                </li>
+                            </ul>
+                        </div>
+                        </div>
+                        <!-- End Park Information Page --> 
+
+                        <div class="map" id="col-xs-12 col-lg-8">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11645.167569209969!2d-77.5715916!3d43.1403995!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xdc8d9de86d9ff017!2sCobbs+Hill+Park!5e0!3m2!1sen!2sus!4v1462455696905" width="100%" height="70%"frameborder="1" style="border:0; margin-top: 20px;" allowfullscreen></iframe>
+                        </div>
+                        
+                        <div class="rtt">
+                            <a href="#top">Top of Page</a>
+                        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        <!-- Begginging Letchworth Section --> 
+                        <section class="row">
+                            <div class="container">
+                            <h2 id="letchworth">Letchworth</h2>
+                            
+                            <div class="row col-12">
+                            <div class="container">
+                            <figure>
+                                <img class="sm_img" src="img/letchworth.jpg" alt="letchworth">
+                                <figcaption><a href="http://tomstirrphotography.com/autumn-images-at-letchworth-state-park">Image Source</a></figcaption>
+                            </figure>
+                            </div>
+                            </div>
+                            
+                            <div class="desc">
+                            <p>
+                            Commonly referred to as the American Grand Canyon, Letchworth State Park stretches across 14,350 acres of land along the Genesee River. At the height of the park are three waterfalls, the tallest of which is 107 feet in height (Source). The waterfall and river have carved their way through the park to create an exquisite landscape and countless breathtaking views. </p>
+
+                            <p> In addition to the amazing views, the park offers many activities for visitors to enjoy. From Swimming pools, fishing areas, hiking trails, hot ballooning, and even white-water rafting and canoeing there are activities that everyone can enjoy (Source). Additionally the park staff organize special events throughout the year from festivals, to guided tours and even arts and crafts (Source). As such the park is one of the most diverse and amazing parks in the region and is only a short 40 minute drive from Rochester Institute of Technology.</p>
+
+                            <p>Catering to a variety of interests the park offers campsites, both as plots of land for those who are gonna do it the old-fashioned way and set up a tent for their stay there.  If you are a more seasoned camper, there are even campsites with water and electricity connections for campers</p>
+                            </div>
+                            </div>
+                        </section>
+                        <!-- End Letchworth Section --> 
+                        <div class="rtt">
+                            <a href="#top">Top of Page</a>
+                        </div>
+
+                    </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        <!-- /#page-content-wrapper -->
+        </div>
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
+    <!-- Google Maps API --> 
+    <script src="https://maps.googleapis.com/maps/api/js" async defer></script>
+
+</body>
+
+</html>
