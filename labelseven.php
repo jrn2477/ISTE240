ebd612cc05e0f13bf8cb21904e8ac871b79aa1ee
sricci20 <ricci.steven@ymail.com>
2016-05-05 11:30:28 -0400

    Update labelseven.php

--- labelseven.php
+++ labelseven.php
@@ -73,8 +73,8 @@
                         <h2>Label Seven Eatery and Bar</h2>
 
                         <div class="foodpics">
-							<img src="images/labelpic1.jpg">
-							<img src="images/labelpic2.jpg">
+							<img src="img/labelpic1.jpg">
+							<img src="img/labelpic2.jpg">
 						</div>
 						
                         <p><b>Location:</b> 50 State St, Pittsford NY<br>
