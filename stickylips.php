99c32133aed17a4ac984bf5a7bb06ee4629020f5
sricci20 <ricci.steven@ymail.com>
2016-05-05 11:32:28 -0400

    Update stickylips.php

--- stickylips.php
+++ stickylips.php
@@ -73,8 +73,8 @@
                         <h2>Sticky Lips BBQ</h2>
 
                         <div class="foodpics">
-							<img src="images/lipspic1.jpg">
-							<img src="images/lipspic2.jpg">
+							<img src="img/lipspic1.jpg">
+							<img src="img/lipspic2.jpg">
 						</div>
 						
                         <p><b>Location:</b> 830 Jefferson Rd, Henrietta NY<br>
