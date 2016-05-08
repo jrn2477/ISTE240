190b79780d2f419ff62ebbec1e521ddeb4e839bd
VMcCormick <vrm9839@rit.edu>
2016-05-08 12:23:08 -0400

    New Eastview mall file

---
+++ eastview.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Team 1
+    ">
+
+    <title>Tigers Explore | Shopping</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+    <!-- Flamenco Font --> 
+    <link href='https://fonts.googleapis.com/css?family=Flamenco' rel='stylesheet' type='text/css'>
+    <!-- Volkorn Font --> 
+    <link href='https://fonts.googleapis.com/css?family=Vollkorn' rel='stylesheet' type='text/css'>
+    <!-- Custom Stylesheet --> 
+    <link href="css/custom-style.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+        
+        <!-- Sidebar 
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">Tiger's Explore</a>
+                </li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Nightlife</a></li>
+                <li><a href="#">Day Trips</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Support</a></li>
+            </ul>
+        </div>
+        /#sidebar-wrapper -->
+
+        <?php 
+            include "nav_menu.php"; 
+        ?>
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+
+            <!-- Header --> 
+            <div class="row">
+            <div class="container-fluid"> 
+                <div class="container-fluid col-lg-1">
+                    <!-- menu-button --> 
+                    <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">
+                        <span class="glyphicon glyphicon-menu-hamburger"></span>
+                    </a>
+                </div><!-- /menu-button --> 
+                <div class="11">
+                    <!-- Site Title & Subtitle --> 
+                    <h1 id="top"><!-- Start Title--><b>Tiger's Explore </b><!-- End Title-->
+                    <!-- Start subtitle-->Shopping<!-- End Subtitle-->
+                    </h1>
+                </div>
+            </div>
+            </div>
+            <!-- End Header--> 
+
+
+            <div class="container">
+            
+
+
+            
+                <div class="container-fluid"> 
+                    <div class="row">
+                    <div class="sec_menu col-12">
+                        <ul>
+                            <li class="sm_item"><a class="sec_menu_links"href="shopping.php">Marketplace Mall</a></li>
+                            <li class="sm_item"><a class="sec_menu_links"href="eastview.php">Eastview Mall</a></li>
+                            <li class="sm_item"><a class="sec_menu_links"href="#">Greece Ridge Mall</a></li>
+                        </ul>
+                    </div>
+                    </div>
+
+
+
+                        <div class="row">
+                                                
+                        <div class="row">
+                        <div class="col-xs-12 col-lg-10">
+
+                            <div class="row">
+                            <h2 id="Cobbs">Eastview</h2>
+                            </div>
+                            
+                            <div class="row">
+                            <div class="container col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                <figure>
+                                    <img class="sm_img" width="90%" src="img/eastview.jpg" alt="Marketplace Mall">
+                                    <figcaption><a href="http://media-cache-ak0.pinimg.com/736x/94/81/55/9481558c45ca2b1f702e0172bb0a297a.jpg">Image Source</a></figcaption>
+                                </figure>
+                            </div>
+                            </div>
+                            
+                            <!-- Eastview Mall Description Start --> 
+                            <div class="row ch_desc">
+                            <h3>About the Eastview Mall</h3>
+
+                            <p>
+                                The Eastview Mall caters to a wealthier demographic with its high-end store options and elegant infrastructure. The interior of the mall is decorated with columns and peaceful fountains. This mall is home to some uncommon shopping destinations such as an Apple store as well as an L.L. Bean. It is located about 15 miles southeast of campus which is about a 30 minute drive.
+                            </p>
+
+                            </div>
+                            <!-- End Eastview Mall Description  --> 
+
+
+                        <!-- New Row for the Mall Information --> 
+                        <div class="row">
+                        <div class="col-xs-12 col-lg-4">
+                            <h3>Popular/High-End Stores:</h3>
+
+                            <ul>
+                                <li>Anthropologie</li>
+                                <li>Apple Store</li>
+                                <li>Best Buy</li>
+                                <li>Forever 21</li>
+                                <li>Lord & Taylor</li>
+                                <li>Swarovski</li>
+                                <li>LL Bean</li>
+                                <li>Pottery Barn</li>
+                                <li>Michael Kors</li>
+                            </ul>
+                        </div>
+
+                        <div class="col-xs-12 col-lg-4">
+                            <h3>Mall Information:</h3>
+
+                            <ul>
+                                <li>Phone: (585)-223-4420</li>
+                                <li>Address: 7979 Pittsford Victor Rd, Victor, NY 14564 </li>
+                            </ul>
+                        </div>
+
+                        <div class="col-xs-12 col-lg-4">
+                            <h3>Hours of Operation:</h3>
+
+                            <ul>
+                                <li>Sunday: 11AM–6PM</li>
+                                <li>Monday: 10AM–9:30PM</li>
+                                <li>Tuesday: 10AM–9:30PM</li>
+                                <li>Wednesday: 10AM–9:30PM</li>
+                                <li>Thursday: 10AM–9:30PM</li>
+                                <li>Friday: 10AM–9:30PM</li>
+                                <li>Saturday: 10AM–9:30PM</li>
+                            </ul>
+                        </div>
+                        </div>
+                        <!-- End Mall Information Page --> 
+
+                        <div class="map" id="col-xs-12 col-lg-8">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2916.6470705330157!2d-77.44513358427244!3d43.02781527914778!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d133e7413dc2e9%3A0x33b6bd8bcc44d41c!2sEastview+Mall!5e0!3m2!1sen!2sus!4v1462722966014" width="100%" height="70%"frameborder="1" style="border:0; margin-top: 20px;" allowfullscreen></iframe>
+                        </div>
+                        
+                        <div class="rtt">
+                            <a href="#top">Top of Page</a>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        <!-- /#page-content-wrapper -->
+        </div>
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
+    <!-- Google Maps API --> 
+    <script src="https://maps.googleapis.com/maps/api/js" async defer></script>
+
+</body>
+
+</html>
