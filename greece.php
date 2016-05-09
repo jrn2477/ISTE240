c7c01e96dd8e842413a17f11d8b704edf6f1a486
VMcCormick <vrm9839@rit.edu>
2016-05-08 20:49:59 -0400

    Greece Ridge Mall Page

---
+++ greece.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Team 1
+    ">
+
+    <title>Tigers Explore | Shopping</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+    <!-- Flamenco Font --> 
+    <link href='https://fonts.googleapis.com/css?family=Flamenco' rel='stylesheet' type='text/css'>
+    <!-- Volkorn Font --> 
+    <link href='https://fonts.googleapis.com/css?family=Vollkorn' rel='stylesheet' type='text/css'>
+    <!-- Custom Stylesheet --> 
+    <link href="css/custom-style.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+        
+        <!-- Sidebar 
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">Tiger's Explore</a>
+                </li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Nightlife</a></li>
+                <li><a href="#">Day Trips</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Support</a></li>
+            </ul>
+        </div>
+        /#sidebar-wrapper -->
+
+        <?php 
+            include "nav_menu.php"; 
+        ?>
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+
+            <!-- Header --> 
+            <div class="row">
+            <div class="container-fluid"> 
+                <div class="container-fluid col-lg-1">
+                    <!-- menu-button --> 
+                    <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">
+                        <span class="glyphicon glyphicon-menu-hamburger"></span>
+                    </a>
+                </div><!-- /menu-button --> 
+                <div class="11">
+                    <!-- Site Title & Subtitle --> 
+                    <h1 id="top"><!-- Start Title--><b>Tiger's Explore </b><!-- End Title-->
+                    <!-- Start subtitle-->Shopping<!-- End Subtitle-->
+                    </h1>
+                </div>
+            </div>
+            </div>
+            <!-- End Header--> 
+
+
+            <div class="container">
+            
+
+
+            
+                <div class="container-fluid"> 
+                    <div class="row">
+                    <div class="sec_menu col-12">
+                        <ul>
+                            <li class="sm_item"><a class="sec_menu_links"href="shopping.php">Marketplace Mall</a></li>
+                            <li class="sm_item"><a class="sec_menu_links"href="eastview.php">Eastview Mall</a></li>
+                            <li class="sm_item"><a class="sec_menu_links"href="greece.php">Greece Ridge Mall</a></li>
+                        </ul>
+                    </div>
+                    </div>
+
+
+
+                        <div class="row">
+                                                
+                        <div class="row">
+                        <div class="col-xs-12 col-lg-10">
+
+                            <div class="row">
+                            <h2 id="Cobbs">Greece Ridge</h2>
+                            </div>
+                            
+                            <div class="row">
+                            <div class="container col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                <figure>
+                                    <img class="sm_img" width="90%" src="img/GreeceRidge.jpg" alt="Marketplace Mall">
+                                    <figcaption><a href="http://www.themallatgreeceridge.com/Data/Images/GreeceRidgeoutside.jpg">Image Source</a></figcaption>
+                                </figure>
+                            </div>
+                            </div>
+                            
+                            <!-- Greece Ridge Mall Description Start --> 
+                            <div class="row ch_desc">
+                            <h3>About the Mall at Greece Ridge</h3>
+
+                            <p>
+                                Located about 20 minutes north of campus, the Mall at Greece Ridge is an average mall including 10 big box retailers and over 140 small businesses. Some of the popular stores include: Bed, Bath and Beyond, Dick’s Sporting Goods, and Barnes & Noble. A unique place at this mall is Glow Golf. Mini-golf is played in a psychedelic, glow-in-the-dark environment.  For RIT students, this is not a very popular mall since the Marketplace Mall is right around the corner. 
+                            </p>
+
+                            </div>
+                            <!-- End Greece Ridge Mall Description  --> 
+
+
+                        <!-- New Row for the Mall Information --> 
+                        <div class="row">
+                        <div class="col-xs-12 col-lg-4">
+                            <h3>Popular Stores:</h3>
+
+                            <ul>
+                                <li>Bar Louie</li>
+                                <li>Barnes & Noble</li>
+                                <li>Bed Bath & Beyond</li>
+                                <li>Forever 21</li>
+                                <li>Glow Golf</li>
+                                <li>Macy's</li>
+                                <li>Target</li>
+                                <li>TGI Friday's</li>
+                                
+                            </ul>
+                        </div>
+
+                        <div class="col-xs-12 col-lg-4">
+                            <h3>Mall Information:</h3>
+
+                            <ul>
+                                <li>Phone: (585)-225-0430</li>
+                                <li>Address: 271 Greece Ridge Center Dr, Rochester, NY 14626 </li>
+                            </ul>
+                        </div>
+
+                        <div class="col-xs-12 col-lg-4">
+                            <h3>Hours of Operation:</h3>
+
+                            <ul>
+                                <li>Sunday: 11AM–6PM</li>
+                                <li>Monday: 10AM–9:30PM</li>
+                                <li>Tuesday: 10AM–9:30PM</li>
+                                <li>Wednesday: 10AM–9:30PM</li>
+                                <li>Thursday: 10AM–9:30PM</li>
+                                <li>Friday: 10AM–9:30PM</li>
+                                <li>Saturday: 10AM–9:30PM</li>
+                            </ul>
+                        </div>
+                        </div>
+                        <!-- End Mall Information Page --> 
+
+                        <div class="map" id="col-xs-12 col-lg-8">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2908.158486146253!2d-77.69329008451675!3d43.20616507913921!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d6b17b8968e0d1%3A0xdfc40c584465f549!2sThe+Mall+at+Greece+Ridge!5e0!3m2!1sen!2sus!4v1462753667856" width="100%" height="70%"frameborder="1" style="border:0; margin-top: 20px;" allowfullscreen></iframe>
+                        </div>
+                        
+                        <div class="rtt">
+                            <a href="#top">Top of Page</a>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        <!-- /#page-content-wrapper -->
+        </div>
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
+    <!-- Google Maps API --> 
+    <script src="https://maps.googleapis.com/maps/api/js" async defer></script>
+
+</body>
+
+</html>
